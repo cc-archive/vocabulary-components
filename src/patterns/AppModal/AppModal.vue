3dcf794bf8292a214b87f7d09e463b824d651b11
Muluh MG Godson <godsonmuluh@gmail.com>
2022-01-24 19:33:41 +0100

    [FontAwesome] import fa vue component

--- src/patterns/AppModal/AppModal.vue
+++ src/patterns/AppModal/AppModal.vue
@@ -31,7 +31,7 @@
   import { FocusTrap } from 'focus-trap-vue'
   import { library } from '@fortawesome/fontawesome-svg-core'
   import { faTimes } from '@fortawesome/free-solid-svg-icons'
-  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
   library.add(faTimes)
 
   export default {
