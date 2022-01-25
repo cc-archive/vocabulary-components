edec7d8a5dcbeee0b07571ab2598bc8a0ec79929
Anik Das <anikdas0811@gmail.com>
2022-01-25 21:10:18 +0530

    chore: nuke storybook and dependencies
    - added nvmrc for usage with nvm
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ["./meta/*.stories.*", "../src/**/*.stories.*"],
-  addons: [
-    "@storybook/addon-docs",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-  ],
-};
