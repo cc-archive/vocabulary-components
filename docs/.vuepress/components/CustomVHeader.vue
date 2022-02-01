5ebae9d84eb3f620d7bf9c869dd1db9eb85c3f3d
Anik Das <anikdas0811@gmail.com>
2022-02-01 20:18:18 +0530

    feat: header component in its page
    Co-authored-by: Dhruv Bhanushali <dhruv_b@live.com>
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ docs/.vuepress/components/CustomVHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <VHeader>
+    <template #logo>
+      <img :src="CCSearchLogo" alt="The CC search logo" />
+    </template>
+    <template #menu-items>
+      <NavItem tag="a" href="/whatever" label="Item One" />
+      <NavItem
+        tag="a"
+        href="/whatever"
+        label="Item External"
+        :isExternal="true"
+      />
+      <NavDropdown label="Item Three">
+        <NavItem tag="a" href="/whatever" label="Item Three A" />
+        <NavItem tag="a" href="/whatever" label="Item Three B" />
+      </NavDropdown>
+    </template>
+  </VHeader>
+</template>
+<script>
+import CCSearchLogo from "@creativecommons/cc-assets/logos/products/search.svg?inline";
+export default {
+  data() {
+    return {
+      CCSearchLogo,
+    };
+  },
+};
+</script>
--- docs/patterns/vheader.md
+++ docs/patterns/vheader.md
@@ -1,3 +1,7 @@
 ---
 title: VHeader
 ---
+
+## Sample Header
+
+<CustomVHeader />
